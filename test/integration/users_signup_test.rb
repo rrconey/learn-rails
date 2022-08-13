531df8b2518d152f345d4bc9f190c45a3ef59f33
Roscoe <rrconey@github.com>
2022-08-13 14:51:55 +0000

    User signup completed

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+  end
+
+
+  test "valid user signup info" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name:  "Benjamin",
+                                         email: "benny@invalid.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    # assert flash.success, "Welcome to the Sample App!"
+  end
+
+end
